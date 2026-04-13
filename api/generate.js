6b6dd4008f1164560db77203736dfc99dae990f2
FA-AlOtaibi <31261131+FA-AlOtaibi@users.noreply.github.com>
2026-04-13 22:22:01 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
         const { prompt, system } = req.body;
         const apiKey = process.env.GEMINI_API_KEY;
 
-       const url = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${apiKey}`;
-
+       const url = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+        
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
